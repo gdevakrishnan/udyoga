3e976418f0f5739417f9b27e2a24720971c16428
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-12-03 13:37:22 +0530

    Deploy

--- backend/udyoga/settings.py
+++ backend/udyoga/settings.py
@@ -93,9 +93,9 @@ DATABASES = {
         "PASSWORD": os.getenv('DB_PASSWORD'),
         "HOST": os.getenv('DB_HOST'),
         "PORT": os.getenv('DB_PORT'),
-        "OPTIONS": {
-            "sslmode": os.getenv('DB_SSLMODE'),
-        },
+        # "OPTIONS": {
+        #     "sslmode": os.getenv('DB_SSLMODE'),
+        # },
     }
 }
 
