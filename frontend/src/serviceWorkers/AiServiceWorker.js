987156987bbda385e7449d6d24b68ddb6589186a
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-12-09 10:06:39 +0530

    Deploy

--- frontend/src/serviceWorkers/AiServiceWorker.js
+++ frontend/src/serviceWorkers/AiServiceWorker.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1/ai/";
+const BASE_URL = `${import.meta.env.BASE_URL}ai/`;
 
 // ---------------------------
 // Scrape JD
--- frontend/src/serviceWorkers/AuthServiceWorker.js
+++ frontend/src/serviceWorkers/AuthServiceWorker.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api/v1/accounts/";
+const BASE_URL = `${import.meta.env.BASE_URL}accounts/`;
 
 // ---------------------------
 // REGISTER
